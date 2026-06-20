713435f4e86a76e56ddbe48ddb4e097e8d923db7
pardhu <pardhu1502@gmail.com>
2026-06-20 13:04:47 +0530

    written login service

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -4,6 +4,9 @@ import prisma from "../config/prisma";
 import type { RegisterInput } from "../types/auth.types";
 import { AppError } from "../utils/AppError";
 
+import jwt from "jsonwebtoken";
+import type { LoginInput } from "../types/auth.types";
+
 export const registerUser = async (
     data:RegisterInput
 )=>{
@@ -35,4 +38,53 @@ return{
     name: user.name,
     email:user.email,
 };
+};
+
+export const loginUser = async (
+  data: LoginInput
+) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: data.email,
+    },
+  });
+
+  if (!user) {
+    throw new AppError(
+      "Invalid email or password",
+      401
+    );
+  }
+
+  const isPasswordValid = await bcrypt.compare(
+      data.password,
+      user.passwordHash
+    );
+
+  if (!isPasswordValid) {
+    throw new AppError(
+      "Invalid email or password",
+      401
+    );
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user.id,
+      email: user.email,
+    },
+    process.env.JWT_SECRET as string,
+    {
+      expiresIn: "7d",
+    }
+  );
+
+  return {
+    token,
+    user: {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+    },
+  };
 };
--- server/src/validators/auth.validator.ts
+++ server/src/validators/auth.validator.ts
@@ -12,3 +12,12 @@ export const registerSchema = z.object({
     .string()
     .min(6,"Password must be at least 6 characters"),
 });
+
+export const loginSchema =z.object({
+    email: z
+    .email("Invalid email address"),
+
+    password:z
+    .string()
+    .min(1,"Password is required"),
+});
