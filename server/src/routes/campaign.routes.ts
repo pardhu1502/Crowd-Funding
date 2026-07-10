f2b24845953e53aaf0f9dece08d692513ce87684
pardhu <pardhu1502@gmail.com>
2026-07-10 16:00:00 +0530

    implemented authorization middleware in update and delete routes

--- server/src/routes/campaign.routes.ts
+++ server/src/routes/campaign.routes.ts
@@ -7,13 +7,15 @@ import { getCampaigns,
 } from "../controllers/campaign.controller";
 import { validate } from "../middleware/validate.middleware";
 import { createCampaignSchema,updateCampaignSchema, } from "../validators/campaign.validator";
+import { authenticate } from "../middleware/auth.middleware";
+import { isCampaignOwner } from "../middleware/campaign-owner.middleware";
 
 const router = Router();
 
 router.get("/", getCampaigns);
 router.post("/", validate(createCampaignSchema), createCampaign);
 router.get("/:id", getCampaignById);
-router.patch("/:id", validate(updateCampaignSchema), updateCampaign);
-router.delete("/:id", deleteCampaign);
+router.patch("/:id", authenticate, isCampaignOwner, validate(updateCampaignSchema), updateCampaign);
+router.delete("/:id", authenticate, isCampaignOwner, deleteCampaign);
 
 export default router;
